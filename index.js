9653d99b2d2fe3f03b1e8bcbfea64d25c20baa74
hassehulabeck <hansbudandersson@gmail.com>
2019-04-08 20:20:13 +0200

    Products as objects

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+// Ett sätt att skriva en component...
 const Product = {
-    props: ['product'],
-    template: `
+        props: ['product'],
+        template: `
         <div>
-            <p> {{ this.product }} </p>
+            <p> {{ this.product.name }} </p>
+            <p v-if="this.product.price"> {{ this.product.price }} </p>
         </div>
     `
-}
-
+    }
+    // ...och ett annat.
 Vue.component('product-list', {
     props: ['title', 'products'],
     components: {
@@ -24,15 +26,21 @@ Vue.component('product-list', {
     `
 })
 
-
+// Här är "root-komponenten", dvs själva appen, 
+// den som är kopplad till ett element på sidan.
 const app = new Vue({
     el: "#app",
     data: {
-        products: [
-            "Lampa",
-            "Stol",
-            "Soffa",
-            "Bord"
+        products: [{
+                name: "Lampa",
+                price: 200
+            },
+            {
+                name: "Stol",
+                price: 2100
+            },
+            { name: "Soffa" },
+            { name: "Bord" }
         ],
         varukorg: []
     },
